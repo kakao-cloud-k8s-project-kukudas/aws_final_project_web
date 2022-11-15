098cb617c26b2ec9a79cada8735763f60f058ac3
Bogeun <70618223+qhrms97@users.noreply.github.com>
2022-11-15 15:55:15 +0900

    config/urls : admin 주소 추가

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import include
 import Signup.views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path("signup/input", Signup.views.base),
     path("signup/getPost", Signup.views.getPost),
     path('signin/', include('Signin.urls')),
