d62fc10e2a114d5bd027aec06feead347334c5b3
Bogeun <70618223+qhrms97@users.noreply.github.com>
2022-11-28 11:53:57 +0900

    [Local] 회원가입, 로그인 암호화 이슈 해결 (간소화)

--- Account/views.py
+++ Account/views.py
@@ -5,6 +5,10 @@ from Account.models import User_info, User
 from django.contrib import auth
 from django.contrib.auth import authenticate, login
 
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+
+
 from .forms import SignupForm
 from .forms import SigninForm
 
@@ -17,21 +21,20 @@ def signup(request):
     elif request.method=='POST':
         signup_form=SignupForm(request.POST)
         if signup_form.is_valid():
-            ## account user 테이블에 추가
-            # user/pw insert
-            user = User.objects.create_user(
-                username=signup_form.company_name,
-                password=signup_form.password
-            )
-            auth.login(request, user)
-            # user info insert
-            user = User_info(
-                company_name=signup_form.company_name,
-                password=signup_form.password,
-                company_initial=signup_form.company_initial
-            )
-            user.save()
-            return redirect('/account/signin')
+            username = request.POST.get('company_name')
+            company_initial = request.POST.get('company_initial')
+            password = request.POST.get('password')
+
+            try:
+                User.objects.get(username=username)
+                messages.error(request, '이미 존재하는 아이디입니다.')
+                return redirect('/account/signup')
+            except ObjectDoesNotExist:
+                User.objects.create_user(username=username, password=password)
+                user_info = User_info(company_name=username, company_initial=company_initial)
+                user_info.save()
+                messages.success(request, '회원가입에 성공하였습니다.')
+                return redirect('/account/signin')
         else:
             context['forms']=signup_form
             if signup_form.errors:
@@ -47,17 +50,19 @@ def signin(request):
     elif request.method=='POST':
         signin_form=SigninForm(request.POST)
         if signin_form.is_valid():
-            username = request.POST['company_name']
-            password = request.POST['password']
+            username = request.POST.get('company_name')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
 
-            user = authenticate(username=username, password=password)
+            # 인증된 사용자경우
             if user is not None:
                 login(request, user)
-
-            # 세션 관리
-            request.session['login_session']=signin_form.login_session
-            request.session.set_expiry(0) # 브라우저 창 닫으면 삭제되기, 14일 보관
-            return redirect('/home')
+                request.session['login_session'] = signin_form.login_session
+                request.session.set_expiry(0)  # 브라우저 창 닫으면 삭제되기, 14일 보관
+                return redirect('/home')
+            else:
+                messages.error(request, 'ID 혹은 비밀번호 오류입니다.')
+                return redirect('/account/signin')
         else:
             context['forms']=signin_form
             if signin_form.errors:
